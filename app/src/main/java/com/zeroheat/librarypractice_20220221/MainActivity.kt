5bf9d0ede0068f8d23a8cd5025cf45394df96130
zeroheat0930 <dongjunkorea@naver.com>
2022-02-21 11:52:22 +0900

    사진크게보기추가

--- app/src/main/java/com/zeroheat/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/zeroheat/librarypractice_20220221/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.zeroheat.librarypractice_20220221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+//        버튼 이외의, TextView, ImageView, LinearLayout 등등도 setOnClickListener로 이벤트 처리가 가능함.
+
+        imgProfile.setOnClickListener{
+
+//            사진을 크게보는 액티비티로 이동
+
+            val myIntent = Intent(this, VIewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+    }
+
+   fun setValues(){
+
+
+    }
+
 }
