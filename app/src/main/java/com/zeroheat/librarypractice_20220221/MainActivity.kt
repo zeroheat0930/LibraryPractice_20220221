8c9d3168d1ebd63f8c821b5250fa30d6ef2ef618
zeroheat0930 <dongjunkorea@naver.com>
2022-02-21 14:52:06 +0900

    TedPermission 라이브러리 설치, PermissionListener 를 이용해서 - 승인시/거부시의 대응방안 미리 작성

--- app/src/main/java/com/zeroheat/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/zeroheat/librarypractice_20220221/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.zeroheat.librarypractice_20220221
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnCall.setOnClickListener {
+
+//             권한 승인 여부에 따른, 행동 방안을 작성해서 => pl 변수에 담아두자.
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+//                    승인이 OK일때 할 행동.
+                    val myUri = Uri.parse("tel:01040660806")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                      거절되었을때 할 행동.
+                    Toast.makeText(this@MainActivity, "권한이 거절되어, 통화가 불가능합니다.", Toast.LENGTH_SHORT )
+                        .show()
+                }
+
+            }
+
+
+
+        }
+
 
 //        버튼 이외의, TextView, ImageView, LinearLayout 등등도 setOnClickListener로 이벤트 처리가 가능함.
 
